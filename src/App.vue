d84ff11c35cdfe8abc46223480858ef28c0b0a9f
Юлиана Анисифорова <yulianaanisiforova@gmail.com>
2025-07-13 18:44:26 +0300

    card, cardlist component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
 import Header from './components/Header.vue'
+import CardList from './components/CardList.vue'
 </script>
 
 <template>
-    <div class="w-4/5 m-auto bg-white h-screen rounded-xl shadow-xl mt-14">
+    <div class="w-4/5 m-auto bg-white rounded-xl shadow-xl mt-14">
         <Header />
+        <div class="p-10">
+
+            <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+            <CardList />
+        </div>
     </div>
+
 </template>
 
 <style scoped>
